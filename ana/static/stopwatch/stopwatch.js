6500919833eb1843a2bbf9836f561370a771d7f4
Brian <briancomfort06@gmail.com>
2023-11-03 15:07:17 -0700

    testing

--- ana/static/stopwatch/stopwatch.js
+++ ana/static/stopwatch/stopwatch.js
@@ -13,14 +13,14 @@ function formatTime(seconds, milliseconds) {
 function updateTimer() {
     timerElement.innerHTML = formatTime(timeLeft, milliseconds);
     if (timeLeft === 10 && milliseconds === 0) {
-        beepSound.play();
+        clickSound.play();
     }
     if (timeLeft === 5 && milliseconds === 0) {
-        beepSound.play();
+        clickSound.play();
         setTimeout(() => beepSound.play(), 500);
     }
     if (timeLeft === 0 && milliseconds === 0) {
-        beepSound.play();
+        clickSound.play();
         setTimeout(() => beepSound.play(), 500);
         setTimeout(() => beepSound.play(), 1000);
         clearInterval(timerInterval);
